d3c40fc84d19c431f79ed39179142367ce0ea886
Vlad-Sebastian Ionescu <ionescu.vlad1@gmail.com>
2021-09-18 22:49:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,31 +3,32 @@ Returneaza true daca n este prim si false daca nu.
 '''
 def is_prime(n):
   # codul vostru aici
-  
+  return True
   
 '''
 Returneaza produsul numerelor din lista lst.
 '''
 def get_product(lst):
   # codul vostru aici
-  
+  pass
   
 '''
 Returneaza CMMDC a doua numere x si y folosind primul algoritm.
 '''
 def get_cmmdc_v1(x, y):
   # codul vostru aici
-  
+  pass
   
 '''
 Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
 '''
 def get_cmmdc_v2(x, y):
   # codul vostru aici
-  
+  pass
   
 def main():
   # interfata de tip consola aici
+  pass
 
 if __name__ == '__main__':
   main()
